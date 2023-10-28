bc83b86d4c591ac7ef60117ae65245b969f9d962
Michał Zuzak <michalzuzak.2@gmail.com>
2023-10-28 21:08:25 +0200

    Highlighting a selected tab in menu

--- wild-event-back-office-application/src/components/dashboard/DashboardComponent.jsx
+++ wild-event-back-office-application/src/components/dashboard/DashboardComponent.jsx
@@ -1,82 +1,76 @@
-import { useNavigate } from "react-router-dom"
-import { useMediaQuery } from "@mui/material"
-
-import {
-	Drawer,
-	Box,
-	Toolbar,
-	List,
-	Divider,
-	ListItem,
-	ListItemButton,
-	ListItemIcon,
-	ListItemText,
-} from "@mui/material"
-
-import LogoutIcon from "@mui/icons-material/Logout"
+import { useNavigate, useLocation } from "react-router-dom";
+import { useMediaQuery, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText, Drawer, Box, Toolbar, List } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
 import itemList from "./DashboardElements";
 import { useUser } from "../../services/useUser";
 
 
 export const DashboardComponent = () => {
-	const isMd = useMediaQuery(theme => theme.breakpoints.up("md"))
-	const isLg = useMediaQuery(theme => theme.breakpoints.up("lg"))
+  const isMd = useMediaQuery((theme) => theme.breakpoints.up("md"));
+  const isLg = useMediaQuery((theme) => theme.breakpoints.up("lg"));
 
-	const drawerWidth = isMd ? 300 : isLg ? 350 : 240
-	const minFontSize = 16
-	const maxFontSizeMd = 30
-	const maxFontSizeLg = 30
-	const fontSizeScale = isMd
-		? maxFontSizeMd
-		: isLg
-			? maxFontSizeLg
-			: minFontSize
+  const drawerWidth = isMd ? 300 : isLg ? 350 : 240;
+  const minFontSize = 16;
+  const maxFontSizeMd = 30;
+  const maxFontSizeLg = 30;
+  const fontSizeScale = isMd
+    ? maxFontSizeMd
+    : isLg
+    ? maxFontSizeLg
+    : minFontSize;
 
-	const navigate = useNavigate();
-	const { user } = useUser();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { user } = useUser();
 
-	const filteredItems = itemList.filter(item => (user.roles).map(role => role.name).includes(item.requiredRole));
+  const filteredItems = itemList.filter((item) =>
+    user.roles.map((role) => role.name).includes(item.requiredRole)
+  );
 
-	return (
-		<>
-			<Drawer
-				variant='permanent'
-				sx={{
-					width: drawerWidth,
-					flexShrink: 0,
-					[`& .MuiDrawer-paper`]: {
-						width: drawerWidth,
-						boxSizing: "border-box",
-					},
-				}}>
-				<Toolbar />
-				<Box sx={{ overflow: "auto" }}>
-					<List>
-						{filteredItems.map(item => (
-							<ListItem key={item.text} disablePadding>
-								<ListItemButton onClick={() => navigate(item.path)}>
-									<ListItemIcon>{item.icon}</ListItemIcon>
-									<ListItemText
-										primary={item.text}
-										sx={{
-											fontSize: fontSizeScale,
-										}}
-									/>
-								</ListItemButton>
-							</ListItem>
-						))}
-					</List>
-					<Divider />
-					<List>
-						<ListItemButton onClick={() => navigate("/logout")}>
-							<ListItemIcon>
-								<LogoutIcon />
-							</ListItemIcon>
-							<ListItemText primary='Logout' />
-						</ListItemButton>
-					</List>
-				</Box>
-			</Drawer>
-		</>
-	)
-}
+  return (
+    <>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          ["& .MuiDrawer-paper"]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: "auto" }}>
+          <List>
+            {filteredItems.map((item) => (
+              <ListItem key={item.text} disablePadding>
+                <ListItemButton
+                  onClick={() => navigate(item.path)}
+                  selected={location.pathname === item.path}
+                >
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText
+                    primary={item.text}
+                    sx={{
+                      fontSize: fontSizeScale,
+                    }}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            <ListItemButton onClick={() => navigate("/logout")}>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItemButton>
+          </List>
+        </Box>
+      </Drawer>
+    </>
+  );
+};
