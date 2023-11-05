8cad6c1f259c50551282a7657e3fd07a7703a900
Michał Zuzak <michalzuzak.2@gmail.com>
2023-11-05 15:20:01 +0100

    Chat skeleton added

--- wild-event-back-office-application/src/components/chat/Chat.jsx
+++ wild-event-back-office-application/src/components/chat/Chat.jsx
@@ -1,56 +1,15 @@
 import React, { useState } from 'react';
-import { MessageBox, ChatItem, ChatList, Input, Button } from 'react-chat-elements';
+import { ChatEngine } from 'react-chat-engine';
 
 const Chat = () => {
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-
-  const handleMessageChange = (event) => {
-    setMessage(event.target.value);
-  };
-
-  const handleSendMessage = () => {
-    setMessages([...messages, { text: message, type: 'text', position: 'right' }]);
-    setMessage('');
-  };
-
   return (
-    <div>
-      <ChatList
-        className='chat-list'
-        dataSource={[
-          {
-            avatar: 'https://facebook.github.io/react/img/logo.svg',
-            title: 'Facebook',
-            subtitle: 'What are you up to?',
-            date: new Date(),
-            unread: 0,
-          },
-        ]}
-      />
-      <div className="chat-window">
-        {messages.map((msg, index) => (
-          <MessageBox
-            key={index}
-            position={msg.position}
-            type={msg.type}
-            text={msg.text}
-          />
-        ))}
-      </div>
-      <Input
-        placeholder="Type here..."
-        value={message}
-        onChange={handleMessageChange}
-        rightButtons={
-          <Button
-            text="Send"
-            onClick={handleSendMessage}
-          />
-        }
-      />
-    </div>
-  );
+    <ChatEngine
+        height="90vh"
+        projectID="77e67795-c1bf-4c04-87e8-ab3029d70730"
+        userName="Marcin"
+        userSecret="WildEvent2023"
+    />
+);
 };
 
 export default Chat;
