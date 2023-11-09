6ab76f924f30e1d33cbdf1954c33791990bc0dfe
Michał Zuzak <michalzuzak.2@gmail.com>
2023-11-09 13:15:50 +0100

    Chat Page adjusted for PubNub config

--- wild-event-back-office-application/src/pages/chatPage/ChatPageDesktop.jsx
+++ wild-event-back-office-application/src/pages/chatPage/ChatPageDesktop.jsx
@@ -1,12 +1,23 @@
 import { DesktopLayout } from "../layout/DesktopLayout"
-import Chat from "../../components/chat/Chat"
+import SimpleChat from "../../components/chat/PubNubChat/simple-chat/simple-chat";
+import { useUser } from "./services/providers/LoggedUserProvider";
+import { PubNubProvider } from "pubnub-react";
+import PubNub from "pubnub";
+
+const currentUser = useUser();
+
+const pubnub = new PubNub({
+	publishKey: process.env.REACT_APP_PUB_KEY,
+	subscribeKey: process.env.REACT_APP_SUB_KEY,
+	uuid: currentUser.id,
+});
 
 export const ChatPageDesktop = () => {
 	return (
-		<>
+		<PubNubProvider client={pubnub}>
 			<DesktopLayout>
-				<Chat />
+				<SimpleChat />
 			</DesktopLayout>
-		</>
+		</PubNubProvider>
 	)
 }
