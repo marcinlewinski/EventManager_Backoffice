f8f3d256ebd793d67790bf2066d00daa41123851
marcinlewinski <marcinlewinski71@gmail.com>
2023-11-23 22:12:18 +0100

    .\src\components\

--- wild-event-back-office-application/src/components/mapConfig/map/Map.jsx
+++ wild-event-back-office-application/src/components/mapConfig/map/Map.jsx
@@ -31,7 +31,7 @@ const Map = ({ mapLocations }) => {
 
         const ref = React.createRef();
         ref.current = document.createElement('div');
-        createRoot(ref.current).render(<Marker feature={location} index={index + 1} />);
+        createRoot(ref.current).render(<Marker className='marker' feature={location} index={index + 1} />);
         new mapboxgl.Marker(ref.current)
           .setLngLat([location.coordinateDTO.longitude, location.coordinateDTO.latitude])
           .setPopup(markerPopup)
